eeec002214620b6c6bfdc31e1f9b49a03b26a1e8
Moselikk <moselikk@gmail.com>
2022-12-14 12:38:50 +0800

    init

---
+++ core/cli/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports = lernaCLI;
+
+function lernaCLI(){
+    console.log('lernaCLI running')
+    return 'This message is from lernaCLI model'
+}
---
+++ core/ming-cli-dev/cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const cli = require("@lerna/cli");
+
+console.log('hello ming-ming-cli-dev is running')
+
+console.log(cli())
+
+
