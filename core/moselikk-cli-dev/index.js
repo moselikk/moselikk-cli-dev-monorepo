d48b1f0e387ac46f1f8f9399843380c188e43b8b
Moselikk <moselikk@gmail.com>
2022-12-15 16:05:17 +0800

    feat: add check env

--- core/moselikk-cli-dev/index.js
+++ core/moselikk-cli-dev/index.js
@@ -1,23 +1,30 @@
 'use strict';
+const path = require('path');
 const semver = require('semver');
 const constant =require('./lib/const');
+const userHome = require('user-home');
+const pathExists = require('path-exists');
 const colors = require('colors');
 const log = require('@moselikk-cli-dev/log');
 const cli = require('@moselikk-cli-dev/cli');
 const pkg = require('./package.json');
 
+let args;
 
 function main() {
   try {
-  checkPkgVersion();
-  checkNodeVersion();
-  checkRoot();
-  log.test('测试', 'log模块正常运行了');
+    checkInputArgs();
+    checkNodeVersion();
+    checkPkgVersion();
+    checkRoot();
+    checkUserHome();
+    checkEnv();
 
-  console.log(cli());
+    log.test('测试', 'log模块正常运行了');
+    log.verbose('debug', '当前为调试模式');
+    console.log(cli());
   } catch (e) {
-
-  log.error(e.message);
+    log.error(e.message);
   }
 
 }
@@ -44,7 +51,52 @@ function checkRoot() {
   const rootCheck = require('root-check');
 
   rootCheck();
-  console.log('uid' + process.getuid());
+}
+
+// 检查用户主目录是否存在
+function checkUserHome() {
+  if (!userHome || !pathExists(userHome)) {
+    throw new Error(colors.red('当前登录用户主目录不存在！'));
+  }
+}
+// 监听调试模式
+function checkInputArgs() {
+  // eslint-disable-next-line global-require
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  checkArgs();
+}
+
+function checkArgs() {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  } else {
+    process.env.LOG_LEVEL = 'info';
+  }
+  log.level = process.env.LOG_LEVEL;
+}
+
+function checkEnv() {
+  // eslint-disable-next-line global-require
+  const dotenv = require('dotenv');
+  const dotenvPath = path.resolve(userHome, '.env');
+  if (pathExists(dotenvPath)) {
+    dotenv.config({
+      path: dotenvPath,
+    });
+  }
+  createDefaultConfig();
+  log.verbose('环境变量', process.env.CLI_HOME_PATH);
+}
+
+function createDefaultConfig() {
+  const cliConfig = {};
+  if (process.env.CLI_HOME) {
+    cliConfig['cliHome'] = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliConfig['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+  process.env.CLI_HOME_PATH = cliConfig.cliHome;
 }
 
 module.exports = main;
--- core/moselikk-cli-dev/lib/const.js
+++ core/moselikk-cli-dev/lib/const.js
@@ -1,5 +1,7 @@
 const LOWEST_NODE_VERSION = '13.0.0';
+const DEFAULT_CLI_HOME = 'ming-code/moselikk-cli-dev-monorepo/core/moselikk-cli-dev/';
 
 module.exports = {
   LOWEST_NODE_VERSION,
+  DEFAULT_CLI_HOME,
 };
